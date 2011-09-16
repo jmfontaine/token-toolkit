7379db5db6ec45a1c753de67a29ef960c7c62261
Jean-Marc Fontaine <jm@jmfontaine.net>
2011-09-16 22:31:49 +0200

    Added missing custom tokens

---
+++ library/PhpTokenToolkit/source/PhpTokenToolkit/Token/Custom/ColonToken.php
@@ -0,0 +1,7 @@
+<?php
+namespace PhpTokenToolkit\Token\Custom;
+
+class ColonToken extends AbstractCustomToken
+{
+    protected $name = 'T_COLON';
+}
---
+++ library/PhpTokenToolkit/source/PhpTokenToolkit/Token/Custom/ParentToken.php
@@ -0,0 +1,7 @@
+<?php
+namespace PhpTokenToolkit\Token\Custom;
+
+class ParentToken extends AbstractCustomToken
+{
+    protected $name = 'T_PARENT';
+}

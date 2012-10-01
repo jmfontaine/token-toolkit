2a0fcb38e3e6be2ec51641b279155db5f488a759
Jean-Marc Fontaine <jm@jmfontaine.net>
2012-10-01 22:49:02 +0200

    Added autoload.php to coding standard checking

--- autoload.php
+++ autoload.php
@@ -11,5 +11,6 @@ set_include_path(__DIR__ . '/src' . PATH_SEPARATOR . get_include_path());
 
 require_once __DIR__ . '/src/TokenToolkit/Autoloader.php';
 use TokenToolkit\Autoloader;
+
 $autoloader = new Autoloader();
 $autoloader->register();

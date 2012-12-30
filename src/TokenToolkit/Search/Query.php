8b7e11973ae3ecff1d8e9dbd97e2be899a711fc3
Jean-Marc Fontaine <jm@jmfontaine.net>
2012-12-30 18:29:27 +0100

    Remove TokenToolkit autoloader

--- src/TokenToolkit/Search/Query.php
+++ src/TokenToolkit/Search/Query.php
@@ -10,11 +10,16 @@
 
 namespace TokenToolkit\Search;
 
+use TokenToolkit\Iterator\TypeFilterIterator;
+use TokenToolkit\Iterator\ContentFilterIterator;
 use TokenToolkit\Search\Result\Result;
 use TokenToolkit\Search\Result\ResultSet;
-use TokenToolkit\Search\Pattern\PatternInterface as SearchPatternInterface;
-use TokenToolkit\Token\TokenInterface;
 use TokenToolkit\TokenStack;
+
+// KLUDGE: We need to include the PHP tokenizer file to make sure that custom constants are defined.
+// Otherwise it may not be loaded yet by the time this file is loaded depending on the order
+// of the classes instanciations.
+require_once __DIR__.'/../Tokenizer/Php.php';
 
 /**
  * Query for PHP token search
@@ -24,35 +29,24 @@ use TokenToolkit\TokenStack;
  * @copyright 2011-2012 Jean-Marc Fontaine <jm@jmfontaine.net>
  * @license http://www.opensource.org/licenses/bsd-license.php BSD License
  */
-class Query
+class Query implements \IteratorAggregate
 {
     const BACKWARD = -1;
     const FORWARD  = 1;
 
+    protected $allowedTypes = array();
+
+    protected $allowedContents = array();
+
     protected $direction = self::FORWARD;
+
+    protected $forbiddenTypes = array();
+
+    protected $forbiddenContents = array();
 
     protected $limit;
 
-    protected $searchPatterns = array();
-
     protected $tokenStack;
-
-    public function __construct(TokenStack $tokenStack, $searchPatterns = array())
-    {
-        if (is_object($searchPatterns)) {
-            $searchPatterns = array($searchPatterns);
-        }
-
-        $this->searchPatterns = $searchPatterns;
-        $this->tokenStack     = $tokenStack;
-    }
-
-    public function addSearchPattern(SearchPatternInterface $searchPattern)
-    {
-        $this->searchPatterns[] = $searchPattern;
-
-        return $this;
-    }
 
     public function getDirection()
     {
@@ -118,23 +112,69 @@ class Query
         return $resultSet;
     }
 
-    public function setDirection($direction)
-    {
-        $this->direction = $direction;
-
-        return $this;
-    }
-
-    public function setLimit($limit)
+    public function limit($limit)
     {
         $this->limit = $limit;
 
         return $this;
     }
 
-    public function setTokenStack(TokenStack $tokenStack)
+    public function searchIn(TokenStack $tokenStack)
     {
         $this->tokenStack = $tokenStack;
+
+        return $this;
+    }
+
+    public function lookBackward()
+    {
+        $this->direction = self::BACKWARD;
+
+        return $this;
+    }
+
+    public function lookForward()
+    {
+        $this->direction = self::FORWARD;
+
+        return $this;
+    }
+
+    public function is($type)
+    {
+        $this->allowedTypes[] = $type;
+
+        return $this;
+    }
+
+    public function isNot($type)
+    {
+        $this->forbiddenTypes[] = $type;
+
+        return $this;
+    }
+
+    public function getIterator()
+    {
+        $iterator = $this->getTokenStack();
+
+        $iterator = new TypeFilterIterator($iterator, $this->allowedTypes, $this->forbiddenTypes);
+
+        $iterator = new ContentFilterIterator($iterator, $this->allowedContents, $this->forbiddenContents);
+
+        return $iterator;
+    }
+
+    public function contentIs($content)
+    {
+        $this->allowedContents[] = $content;
+
+        return $this;
+    }
+
+    public function contentIsNot($content)
+    {
+        $this->forbiddenContents[] = $content;
 
         return $this;
     }
